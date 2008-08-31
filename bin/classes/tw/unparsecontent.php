ead09086f90d3a3957dbf2b176520a76c2d50eae
Isaac Z. Schlueter <i@foohack.com>
2008-08-30 22:43:24 -0700

    Replace assoc arrays with stdClass objects.

--- bin/classes/tw/unparsecontent.php
+++ bin/classes/tw/unparsecontent.php
@@ -3,8 +3,8 @@
 // a sorry stand-in for a template engine.
 class TW_UnParseContent extends TW_Base {
 	protected static function each ($node) {
-		if ($node->content && is_array($node->content)) {
-			$node->content = $node->content['body'];
+		if ($node->content && is_object($node->content)) {
+			$node->content = $node->content->body;
 		} else {
 			$node->content = null;
 		}
