e30371a8a95fbbf118639f7aa024f750a0534af5
Isaac Z. Schlueter <i@foohack.com>
2008-08-30 22:40:35 -0700

    Handle metadata as well as parsing.

--- bin/classes/tw/parsecontent.php
+++ bin/classes/tw/parsecontent.php
@@ -1,8 +1,61 @@
 <?php
 
 class TW_ParseContent extends TW_Base {
+	// protected static function finish ($node) { die(); }
+	
+	private static function getTitle ($node) {
+		// title is either the explicit title or the "name" with any .ext stripped off.
+		return (property_exists($node->content->headers, "title")) ?
+			$node->content->headers->title :
+			mb_convert_case(
+				str_replace('-', ' ', to_object(
+					pathinfo($node->name)
+				)->filename), MB_CASE_TITLE, 'UTF-8'
+			);
+	}
+	private static function getStub ($node) {
+		return (property_exists($node->content->headers, 'stub')) ?
+			$node->content->headers->stub :
+			preg_replace('~[^a-zA-Z0-9%]+~', '-',
+				str_replace('%20', ' ', rawurlencode( strtolower( trim(self::getTitle($node)) ) ) )
+			);
+	}
+	private static function getType ($node) {
+		return (property_exists($node->content->headers, 'type')) ?
+			$node->content->headers->type :
+			( property_exists($node->content->headers, 'date') ? 'blog' : 'static' );
+	}
+	private static function getStatus ($node) {
+		return (property_exists($node->content->headers, 'status')) ?
+			$node->content->headers->status : 'published';
+	}
+	
+	private static function getHeaders ($node) {
+		// get the stub
+		$node->content->headers->title = self::getTitle($node);
+		$node->content->headers->stub = self::getStub($node);
+		
+		if (
+			property_exists($node->content->headers, "date")
+		) if (
+			!($node->content->headers->date = strtotime($node->content->headers->date))
+		) unset($node->content->headers->date);
+
+		$node->content->headers->type = self::getType($node);
+		$node->content->headers->status = self::getStatus($node);
+		$node->name = $node->content->headers->stub;
+		
+		return $node->content->headers;
+	}
+	
 	protected static function each ($node) {
 		$node->content = Parser::parse($node->content);
+		
+		// while we're at it, get some handy stuff.
+		$node->content->headers = self::getHeaders($node);
+		
+		// error_log("Parsing walker: ". $node->path);
+		
 	}
 	public static function walk ($node) { parent::walk($node); }
 }
