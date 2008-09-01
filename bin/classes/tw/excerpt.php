d17dcbe78f6d11470272d18bbf105fece922c884
Isaac Z. Schlueter <i@foohack.com>
2008-08-31 17:01:13 -0700

    Increase terseness.

--- bin/classes/tw/excerpt.php
+++ bin/classes/tw/excerpt.php
@@ -3,14 +3,11 @@
 class TW_Excerpt extends TW_Base {
 	protected static function each ($node) {
 		$body = $node->content->body;
-		if (false !== strpos($body, '<!--more-->')) {
-			$excerpt = explode('<!--more-->', $body);
-			$excerpt = $excerpt[0];
-		} else {
-			$excerpt = strip_tags($body);
-			$excerpt = substr($excerpt, 0, 250);
-		}
-		$node->content->excerpt = trim($excerpt);
+		$node->content->excerpt = trim(
+			(false !== strpos($body, '<!--more-->')) ?
+				array_shift(explode('<!--more-->', $body)) :
+				substr(strip_tags($body), 0, 250)
+		);
 	}
 	public static function walk ($node) { parent::walk($node); }
 }
