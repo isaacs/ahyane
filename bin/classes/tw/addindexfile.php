cb11a20e61be4f20c0dbbecbb32c55a5424aefee
Isaac Z. Schlueter <i@foohack.com>
2008-09-01 01:00:53 -0700

    Just making this a bit cleaner.

--- bin/classes/tw/addindexfile.php
+++ bin/classes/tw/addindexfile.php
@@ -2,11 +2,10 @@
 
 class TW_AddIndexFile extends TW_Base {
 	protected static function each ($node) {
-		if ($node->content) {
-			$node->child( new ContentNode('index.html') );
-			$node->_('index.html')->content = $node->content;
-			$node->content = null;
-		}
+		if (!$node->content) return;
+		$node->child( new ContentNode('index.html') );
+		$node->_('index.html')->content = $node->content;
+		$node->content = null;
 	}
 	public static function walk ($node) { parent::walk($node); }
 }
