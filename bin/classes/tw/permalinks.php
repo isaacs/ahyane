532cbfea20bb1eb31a005cc008df3308270722f2
Isaac Z. Schlueter <i@foohack.com>
2008-09-14 18:29:26 -0700

    Removing the error_log.

--- bin/classes/tw/permalinks.php
+++ bin/classes/tw/permalinks.php
@@ -24,7 +24,6 @@ class TW_Permalinks extends TW_Base {
 	}
 	
 	protected static function each ($node) {
-		error_log("Permalink walker: ". $node->path);
 		if (
 			is_object($node->content) && $node !== self::$root
 		) $node->path = $node->content->headers->permalink = self::getPermalink($node);
