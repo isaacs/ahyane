8647b66d120a9286e1c664f6d8e906d068d0bf15
Isaac Schlueter <isaacs@sistertrain-lm.(none)>
2008-09-27 03:59:16 -0700

    Make a bit more markup.

--- tpl/index.php
+++ tpl/index.php
@@ -1,9 +1,28 @@
-in the template!!
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 
-<pre><?php
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Ahyane - <?php
+	echo $node->header("title", "erm... something?");
+	?></title>
+</head>
+<body>
+	
+	<h1><?php echo $node->header("title", "erm... something?"); ?></h1>
+<?php
 
-print_r($node->content);
+if ($node->header("archive")) {
+	// this is where you'd loop through the posts or something?
+	foreach ($node->content->body as $piece) {
+		echo "<h2>" . $piece->headers->title . "</h2>";
+		echo "<h3>" . $piece->headers->permalink . "</h3>";
+		echo "<pre>" . $piece->excerpt . "</pre>";
+	}
+} else {
+	echo $node->content->body;
+}
 
-?></pre>
-
-ending template!!
+?>
+</body>
+</html>
