e959835e86fb48930f3be8c1cdd1f872d7469fe1
Isaac Schlueter <isaacs@sistertrain-lm.(none)>
2008-10-04 19:37:50 -0700

    Create "unfix" functions.

--- bin/classes/config.php
+++ bin/classes/config.php
@@ -4,18 +4,6 @@ class Config {
 	private static $conf = array();
 	private static $fixed = array();
 	private static $instance = null;
-
-	// Part of me things that Config::_()->foo is just too many special chars in a row..
-	// public function __get ($key) {
-	// 	return self::get($key);
-	// }
-	// public function __set ($key, $val) {
-	// 	return self::set($key, $val);
-	// }
-	// public static function _ () {
-	// 	return self::$instance ? self::$instance : (self::$instance = new self());
-	// }
-	
 	
 	public static function get ($key, $default = null) {
 		return array_key_exists($key, self::$conf) ? self::$conf[$key] : $default;
@@ -33,6 +21,7 @@ class Config {
 		// do it this way so it's a copy, not a leak
 		return self::geta( array_keys(self::$conf) );
 	}
+	
 	public static function set ($key, $val) {
 		$key = trim(strtolower($key));
 		return (
@@ -40,8 +29,11 @@ class Config {
 		) ? null : (self::$conf[$key] = $val);
 	}
 	public static function seta ($arr) {
+		if (!is_array($arr) && !is_object($arr)) return false;
 		foreach ($arr as $k => $v) self::set($k, $v);
+		return true;
 	}
+	
 	public static function fix ($key) {
 		if (!in_array($key, self::$fixed)) self::$fixed[] = $key;
 	}
@@ -51,6 +43,17 @@ class Config {
 	public static function fixAll () {
 		self::fixa(array_keys(self::$conf));
 	}
+	
+	public static function unfix ($key) {
+		if (in_array($key, self::$fixed)) unset( self::$fixed[array_search($key, self::$fixed)] );
+	}
+	public static function unfixa ($arr) {
+		foreach ($arr as $a) self::unfix($a);
+	}
+	public static function unfixAll () {
+		self::$fixed = array();
+	}
+	
 	public static function read ($filename) {
 		return self::seta( Parser::readHeaderFile($filename) );
 	}
