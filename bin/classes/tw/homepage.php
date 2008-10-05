0034c97ea4b228308842e9350f4cd4f47a1970f3
isaacs <isaacs@sistertrain-lm.(none)>
2008-10-05 15:20:11 -0700

    Make the home page configurable.

--- bin/classes/tw/homepage.php
+++ bin/classes/tw/homepage.php
@@ -2,22 +2,41 @@
 
 class TW_HomePage extends TW_Base {
 	private static $posts = array();
+	private static $homepage = null;
+	private static $static_homepage = false;
+	
 	protected static function finish ($node) {
-		$node->content = to_object(array(
-			'headers' => array(
-				'home' => true,
-				'archive' => true
-			),
-			'body' => self::$posts
-		));
+		$node->content = to_object(
+			self::$homepage ?
+			self::$homepage->content :
+			array(
+				'headers' => array('archive' => true),
+				'body' => self::$posts
+			)
+		);
+		$node->content->headers->home = true;
+	}
+	
+	protected static function start ($node) {
+		self::$static_homepage = @Config::get("homepage", false);
 	}
 	
 	protected static function each ($node) {
 		if (
-			!$node->header("permalink") ||
-			$node->header("type") === "static"
+			// found it already
+			self::$homepage
 		) return;
-		self::$posts[] = $node->content;
+		elseif (
+			// looking for a static homepage, and this is it.
+			self::$static_homepage &&
+			$node->name === self::$static_homepage &&
+			$node->header("type") === "static"
+		) self::$homepage = $node;
+		elseif (
+			// this is a blog post, so save it.
+			$node->header("permalink") &&
+			$node->header("type") !== "static"
+		) self::$posts[] = $node->content;
 	}
 	
 	public static function walk ($node) { parent::walk($node); }
