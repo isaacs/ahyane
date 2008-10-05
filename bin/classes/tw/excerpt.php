86f00fa7119b1787a321bca10cbbd130b786f66d
--global <isaacs@sistertrain-lm.(none)>
2008-10-05 02:06:17 -0700

    Specify the excerpt length in the config

--- bin/classes/tw/excerpt.php
+++ bin/classes/tw/excerpt.php
@@ -6,7 +6,7 @@ class TW_Excerpt extends TW_Base {
 		$node->content->excerpt = trim(
 			(false !== strpos($body, '<!--more-->')) ?
 				array_shift(explode('<!--more-->', $body)) :
-				substr(strip_tags($body), 0, 250)
+				substr(strip_tags($body), 0, Config::get("excerptlength"))
 		);
 	}
 	public static function walk ($node) { parent::walk($node); }
