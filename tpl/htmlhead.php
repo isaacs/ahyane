e653e8e449576e352da28e701bf7deafb0c95b65
isaacs <i@foohack.com>
2008-10-06 15:24:57 -0700

    Stylesheets.

--- tpl/htmlhead.php
+++ tpl/htmlhead.php
@@ -2,6 +2,12 @@
 <html lang="en" id="<?php echo str_replace("/", '-', $node->path); ?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/reset-fonts-grids/reset-fonts-grids.css">
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/base/base-min.css">
+	<link rel="stylesheet" type="text/css" href="${TemplateURL}/style.css">
+	<meta name="generator" content="Ahyane">
+	
 	<title><?php
 	echo Config::get("sitename") . ' » ' . (
 		$node->header("home") ? Config::get("SiteDescription") : $node->header("title")
