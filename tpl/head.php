94df3ccc254774cfadb15a8419bffa8b33fe3531
isaacs <i@foohack.com>
2008-10-06 19:52:46 -0700

    Link back to the home page.

--- tpl/head.php
+++ tpl/head.php
@@ -1,5 +1,9 @@
 <div id="hd">
-	<h1><?php echo Config::get("SiteName"); ?></h1>
+	<h1><a href="<?php
+		echo Config::get("URLPrefix");
+	?>" rel="home"><?php
+		echo Config::get("SiteName");
+	?></a></h1>
 	<p><?php echo Config::get("SiteDescription"); ?></p>
 	<?php
 	$node->template("nav.php");
