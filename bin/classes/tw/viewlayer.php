b48246a2eceb451acb6adc8479bbcccd07919769
isaacs <i@foohack.com>
2008-10-09 11:55:42 -0700

    Don't destroy the headers, just overwrite content->body.

--- bin/classes/tw/viewlayer.php
+++ bin/classes/tw/viewlayer.php
@@ -3,15 +3,15 @@
 // a sorry stand-in for a template engine.
 class TW_ViewLayer extends TW_Base {
 	protected static function each ($node) {
-		if ($node->content && is_object($node->content)) {
-			// $node->content = print_r($node->content, 1);
-			ob_start();
-			require(Config::get("template") . '/index.php');
-			$node->content = ob_get_contents();
-			ob_end_clean();
-		} else {
-			$node->content = null;
-		}
+		if ($node->header("viewlayerapplied")) return;
+		if (
+			$node->content &&
+			is_object($node->content) &&
+			property_exists($node->content, "body")
+		) {
+			$node->content->body = $node->template("index.php", true);
+			$node->content->headers->viewlayerapplied = true;
+		} else $node->content = null;
 	}
 	public static function walk ($node) { parent::walk($node); }
 }
