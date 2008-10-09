f1cba458646190ff0fab9398178126ed20b06b55
isaacs <i@foohack.com>
2008-10-09 11:59:09 -0700

    Remove debuggery.

--- bin/classes/tw/addindexfile.php
+++ bin/classes/tw/addindexfile.php
@@ -6,7 +6,6 @@ class TW_AddIndexFile extends TW_Base {
 			!$node->content || !$node->content->body
 		) return;
 		$filename = 'index.' . ($node->header("feed") ? "x" : "ht") . "ml";
-		error_log($node->path. ' ' .$filename);
 		$node->child( new ContentNode($filename) )->content = $node->content->body;
 		$node->content = null;
 	}
