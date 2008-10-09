9bee9e4866bdb849f1447c9276a5b2b7cbba9d20
isaacs <i@foohack.com>
2008-10-09 11:53:08 -0700

    Change the way that config tokens are parsed, to do it in one str_ireplace pass and use an unprintable character as the "blocking" byte.

--- bin/classes/tw/configtokens.php
+++ bin/classes/tw/configtokens.php
@@ -2,19 +2,14 @@
 
 class TW_ConfigTokens extends TW_Base {
 	protected static function each ($node) {
-		if (!$node->content) return;
-		foreach (Config::getAll() as $key => $val) {
-			$node->content = str_ireplace(
-				'\\${'.$key.'}', '$\\{' . $key . '}', $node->content
-			);
-			$node->content = str_ireplace(
-				'${'.$key.'}', $val, $node->content
-			);
-			$node->content = str_ireplace(
-				'$\\{'.$key.'}', '${' . $key . '}', $node->content
-			);
-			
-		}
+		if (!$node->content || !$node->content->body) return;
+		foreach (
+			Config::getAll() as $key => $val
+		) $node->content->body = str_ireplace(
+			array('\\${' . $key . '}', '${' . $key . '}', "$\1{" . $key . "}"),
+			array("$\1{" . $key . "}", $val, '${' . $key . '}'),
+			$node->content->body
+		);
 	}
 	public static function walk ($node) { parent::walk($node); }
 }
