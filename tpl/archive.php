7e0e1a3592951a718470999516f00d7e21e13914
isaacs <i@foohack.com>
2008-10-09 14:19:52 -0700

    Add more semantics.

--- tpl/archive.php
+++ tpl/archive.php
@@ -6,9 +6,37 @@
 		<?php
 		foreach ($node->content->body as $post) {
 			?>
-			<div class="post" id="<?php
+			<div class="<?php
+			$classes = array(
+				'post hentry',
+				'author-' . $node->slugify($post->headers->author),
+				$node->slugify($post->headers->status),
+				'y' . date('Y', $post->headers->date),
+				'm' . date('m', $post->headers->date),
+				'd' . date('d', $post->headers->date),
+				'h' . date('H', $post->headers->date)
+			);
+			foreach ($post->headers->tags as $tag) {
+				$classes[] = 'tag-' . $node->slugify($tag);
+			}
+			echo join(" ", $classes);
+			?>" id="<?php
 				echo $post->headers->slug;
 			?>">
+				<?php
+				if (!empty($post->headers->tags)) {
+					?>
+					<ul class="tags">
+						<?php
+						foreach ($post->headers->tags as $url => $tag) {
+							echo "\n" . '<li><a rel="tag" href="' .
+								$node->urlify($url) . '">' . $tag . '</a></li>';
+						}
+						?>
+					</ul>
+					<?php
+				}
+				?>
 				<h3><a rel="bookmark" href="<?php
 					echo $post->headers->permalink;
 				?>"><?php
