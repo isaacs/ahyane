d0a8eb1d6b966f0d7e3c1c6fa7293b8e8f2dc718
isaacs <i@foohack.com>
2008-10-08 19:20:03 -0700

    Split up the <?xml prolog to not break PHP.

--- tpl/feed.php
+++ tpl/feed.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<' . '?xml' ?> version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
 	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
