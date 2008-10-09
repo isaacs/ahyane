a70adafa84ca3ac6ace9de75c9492a0f1c3d306f
isaacs <i@foohack.com>
2008-10-09 11:56:01 -0700

    Look at the "feed" header to determine index.html or index.xml filename.

--- bin/classes/tw/addindexfile.php
+++ bin/classes/tw/addindexfile.php
@@ -2,9 +2,16 @@
 
 class TW_AddIndexFile extends TW_Base {
 	protected static function each ($node) {
-		if (!$node->content) return;
-		$node->child( new ContentNode('index.html') );
-		$node->_('index.html')->content = $node->content;
+		if (
+			!$node->content || !$node->content->body
+		) {
+			error_log("no content for " . $node->path);
+			var_dump($node->content);
+			return;
+		}
+		$filename = 'index.' . ($node->header("feed") ? "x" : "ht") . "ml";
+		error_log($node->path. ' ' .$filename);
+		$node->child( new ContentNode($filename) )->content = $node->content->body;
 		$node->content = null;
 	}
 	public static function walk ($node) { parent::walk($node); }
