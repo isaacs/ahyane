2b61cbe4c69bac7abc904eae49040890b41ffc93
isaacs <i@foohack.com>
2008-11-03 22:17:06 -0800

    Remove the | between the prev and next links, as Kubrick does not need it.

--- tpl/nav.php
+++ tpl/nav.php
@@ -5,9 +5,6 @@ if ($node->previous || $node->next) {
 		echo '<div class="alignleft"><a class="previous" href="'. $node->previous->href . '">' .
 			$node->previous->title . '</a></div>';
 	}
-	if ($node->previous && $node->next) {
-		echo '<span>|</span>';
-	}
 	if ($node->next) {
 		echo '<div class="alignright"><a class="next" href="'. $node->next->href . '">' .
 			$node->next->title . '</a></div>';
