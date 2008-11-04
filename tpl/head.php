5fd4eab03ca71dad3647748ce04b473f31b6a83c
isaacs <i@foohack.com>
2008-11-03 22:16:10 -0800

    One head only, please.

--- tpl/head.php
+++ tpl/head.php
@@ -1,13 +1,4 @@
 <div id="header">
-	<div id="headerimg" onclick="location.href='http://isaacschlueter.wordpress.com';" style="cursor: pointer;">
-		<h1><a href="http://isaacschlueter.wordpress.com/">Site Title</a></h1>
-		<div class="description">Site Tagline</div>
-	</div>
-</div>
-
-TODO: update the CSS so these look the same.
-
-<div id="header">
 	<h1><a href="${SiteURL}" rel="home">${SiteName}</a></h1>
-	<p>${SiteDescription}</p>
+	<p class="description">${SiteDescription}</p>
 </div>
