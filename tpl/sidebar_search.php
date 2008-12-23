c9adcd6bd1699f8dc55f5a24fdf85e92a239de4f
isaacs <i@foohack.com>
2008-12-22 23:36:37 -0800

    Sidebar search, care of teh googlez.

---
+++ tpl/sidebar_search.php
@@ -0,0 +1,11 @@
+<li>
+	<div>
+		<form name="searchform" id="searchform" method="get" action="http://www.google.com/search">
+			<input type="hidden" name="client" value="ahyane">
+			<label style="position:absolute;top:-99em;left:-99em" for="sidebar_q">Search this site</label>
+			<input type="text" id="sidebar_q" name="q" value="search this site" onfocus="if(this.value==='search this site')this.value=''" onblur="if(this.value==='')this.value='search this site'">
+			<input type=submit style="position:absolute;top:-99em;left:-99em" value="Search">
+			<input type="hidden" name=q value="site:${SiteURL}">
+		</form>
+	</div>
+</li>
