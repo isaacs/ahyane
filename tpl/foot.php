486a235f8ec9da076c6b49de2c3acff324ca2bc7
isaacs <i@foohack.com>
2008-12-22 23:35:20 -0800

    A proper footer.

--- tpl/foot.php
+++ tpl/foot.php
@@ -1,1 +1,9 @@
-foot!
+
+
+<hr>
+<div id="footer">
+	<p>
+		<a href='http://ahyane.com/' rel='generator'>Built using Ahyane</a>
+		<br><a href="${SiteUrl}/feed/">Entries (RSS)</a>	</p>
+</div>
+</div>
